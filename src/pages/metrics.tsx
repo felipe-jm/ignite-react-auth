72e547a9e258a2b425fa44ab04bf35864352ff92
Felipe Jung <felipemattoseu@gmail.com>
2021-05-11 10:50:27 -0400

    Block user in server side if does not have permissions

---
+++ src/pages/metrics.tsx
@@ -0,0 +1,28 @@
+import { withSSRAuth } from "utils/withSSRAuth";
+
+import { setupAPIClient } from "services/api";
+
+const Metrics = () => {
+  return (
+    <>
+      <h1>Metrics</h1>
+    </>
+  );
+};
+
+export const getServerSideProps = withSSRAuth(
+  async (ctx) => {
+    const apiClient = setupAPIClient(ctx);
+    const response = await apiClient.get("/me");
+
+    return {
+      props: {},
+    };
+  },
+  {
+    permissions: ["metrics.list"],
+    roles: ["administrator"],
+  }
+);
+
+export default Metrics;
