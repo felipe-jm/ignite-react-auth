1d013e400c8fbd4a961c499222456340f1833e37
Felipe Jung <felipemattoseu@gmail.com>
2021-05-11 09:27:21 -0400

    Add refresh token strategy

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import Router from "next/router";
 
-import { setCookie, parseCookies } from "nookies";
+import { setCookie, parseCookies, destroyCookie } from "nookies";
 import { api } from "services/api";
 
 type User = {
@@ -29,6 +29,13 @@ export const AuthContext = createContext<AuthContextData>(
   {} as AuthContextData
 );
 
+export function signOut() {
+  destroyCookie(undefined, "nextauth.token");
+  destroyCookie(undefined, "nextauth.refreshToken");
+
+  Router.push("/");
+}
+
 export const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User>(null);
   const isAuthenticated = !!user;
@@ -37,11 +44,16 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     const { "nextauth.token": token } = parseCookies();
 
     if (token) {
-      api.get("/me").then((response) => {
-        const { email, permissions, roles } = response.data;
+      api
+        .get("/me")
+        .then((response) => {
+          const { email, permissions, roles } = response.data;
 
-        setUser({ email, permissions, roles });
-      });
+          setUser({ email, permissions, roles });
+        })
+        .catch(() => {
+          signOut();
+        });
     }
   }, []);
 
@@ -61,12 +73,12 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
       // cookies - persistent and available in server side
 
-      setCookie(undefined, "@nextauth.token", token, {
+      setCookie(undefined, "nextauth.token", token, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
 
-      setCookie(undefined, "@nextauth.refreshToken", refreshToken, {
+      setCookie(undefined, "nextauth.refreshToken", refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: "/",
       });
